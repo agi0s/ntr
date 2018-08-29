82d09153f637fd10bd5276c96b5265d990ed85ed
agi0s <olej.z@icloud.com>
2018-08-29 20:09:04 +0300

    3

---
+++ p1/lss/csh.js
@@ -0,0 +1,22 @@
+//greedy algorithm
+function cash(){
+	let coinsArray = [25, 10, 5, 1],
+		coinsAmount = 0,
+		change;
+
+		do {
+			var change = prompt('Change amount?') * 100;
+		}
+		while( change < 0 || !change );
+
+	
+		coinsArray.forEach( coin => {
+			while( change >= coin ){
+				change -= coin;
+				coinsAmount++;
+			}})
+
+		console.log(coinsAmount);
+}
+
+	
