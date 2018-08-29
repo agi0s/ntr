e6031c37ed8bdab33bc4e99ad9b23b9a17c60b93
agi0s <olej.z@icloud.com>
2018-08-29 17:33:10 +0300

    Initial commit

---
+++ p1/lss/mr.js
@@ -0,0 +1,19 @@
+function mario(number){
+	let space = '',
+		hash = '';
+
+	for (let i = 0; i < number; i++){
+		for( var s = number - 1; s > i; s-- ){
+			space += ' ';
+		}
+
+		for( var h = 0; h <= i + 1; h++){
+			hash += '#';
+		}
+
+		console.log(`${space}${hash}`);
+
+		space = '';
+		hash = '';
+	}
+}
